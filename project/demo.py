90ff8563a9bd8b3813bad5b9d5c700832c88168a
delldu <18588220928@163.com>
2022-10-21 20:21:13 +0800

    Update

--- project/demo.py
+++ project/demo.py
@@ -1,6 +1,3 @@
 import image_dereflection
 
-# image_dereflection.image_client("TAI", "images/*.png", "output")
-# image_dereflection.image_server("TAI")
-
 image_dereflection.image_predict("images/*.png", "output")
--- project/image_dereflection/dereflection.py
+++ project/image_dereflection/dereflection.py
@@ -9,7 +9,7 @@
 # ***
 # ************************************************************************************/
 #
-
+import math
 import numpy as np
 import torch
 import torch.nn.functional as F
@@ -484,7 +484,7 @@ class SIRRModel(nn.Module):
         super(SIRRModel, self).__init__()
         self.netG_T = LRM()
 
-    def forward(self, x):
+    def forward_x(self, x):
         b, c, h, w = x.shape
 
         fake_h = torch.zeros(b, 64, h, w, device=x.device)
@@ -495,3 +495,39 @@ class SIRRModel(nn.Module):
             fake_h, fake_c, fake_t = self.netG_T(x, fake_t, fake_h, fake_c)
 
         return fake_t.clamp(0.0, 1.0)
+
+    def forward(self, x):
+        # Define max GPU/CPU memory -- 4G
+        max_h = 1024
+        max_W = 1024
+        multi_times = 8
+
+        # Need Resize ?
+        B, C, H, W = x.size()
+        if H > max_h or W > max_W:
+            s = min(max_h / H, max_W / W)
+            SH, SW = int(s * H), int(s * W)
+            resize_x = F.interpolate(x, size=(SH, SW), mode="bilinear", align_corners=False)
+        else:
+            resize_x = x
+
+        # Need Zero Pad ?
+        ZH, ZW = resize_x.size(2), resize_x.size(3)
+        if ZH % multi_times != 0 or ZW % multi_times != 0:
+            NH = multi_times * math.ceil(ZH / multi_times)
+            NW = multi_times * math.ceil(ZW / multi_times)
+            resize_zeropad_x = resize_x.new_zeros(B, C, NH, NW)
+            resize_zeropad_x[:, :, 0:ZH, 0:ZW] = resize_x
+        else:
+            resize_zeropad_x = resize_x
+
+        # MS Begin
+        y = self.forward_x(resize_zeropad_x)
+        del resize_zeropad_x, resize_x  # Release memory !!!
+
+        y = y[:, :, 0:ZH, 0:ZW]  # Remove Zero Pads
+        if ZH != H or ZW != W:
+            y = F.interpolate(y, size=(H, W), mode="bilinear", align_corners=False)
+        # MS End
+
+        return y
