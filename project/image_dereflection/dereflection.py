a71e3af69832553ebde890b59756c8e5bd12e12b
delldu <18588220928@163.com>
2022-11-13 19:21:08 +0800

    Update

--- project/image_dereflection/dereflection.py
+++ project/image_dereflection/dereflection.py
@@ -486,10 +486,9 @@ class SIRRModel(nn.Module):
         self.MAX_H = 512
         self.MAX_W = 512
         self.MAX_TIMES = 8
+
         self.netG_T = LRM()
-
         self.load_weights()
-
         self.netG_T.eval()
 
     def load_weights(self, model_path="models/image_dereflection.pth"):
@@ -508,29 +507,3 @@ class SIRRModel(nn.Module):
                 fake_h, fake_c, fake_t = self.netG_T(x, fake_t, fake_h, fake_c)
 
         return fake_t.clamp(0.0, 1.0)
-
-    # def forward(self, x):
-    #     # Need Resize ?
-    #     B, C, H, W = x.size()
-    #     if H > self.MAX_H or W > self.MAX_W:
-    #         s = min(self.MAX_H / H, self.MAX_W / W)
-    #         SH, SW = int(s * H), int(s * W)
-    #         resize_x = F.interpolate(x, size=(SH, SW), mode="bilinear", align_corners=False)
-    #     else:
-    #         resize_x = x
-
-    #     # Need Pad ?
-    #     PH, PW = resize_x.size(2), resize_x.size(3)
-    #     if PH % self.MAX_TIMES != 0 or PW % self.MAX_TIMES != 0:
-    #         r_pad = self.MAX_TIMES - (PW % self.MAX_TIMES)
-    #         b_pad = self.MAX_TIMES - (PH % self.MAX_TIMES)
-    #         resize_pad_x = F.pad(resize_x, (0, r_pad, 0, b_pad), mode="replicate")
-    #     else:
-    #         resize_pad_x = resize_x
-
-    #     y = self.forward_x(resize_pad_x)
-
-    #     y = y[:, :, 0:PH, 0:PW]  # Remove Pads
-    #     y = F.interpolate(y, size=(H, W), mode="bilinear", align_corners=False)  # Remove Resize
-
-    #     return y
